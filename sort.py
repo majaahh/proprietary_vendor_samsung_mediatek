72edd91574016de931dad5f61f044d737cc76a3e
Majaahh <maja.blaszczak@proton.me>
2026-06-05 18:27:45 +0200

    sort: Initial script
    Change-Id: I396a5cdfef46247847417401dc01a37a09dd6ffa
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

---
+++ sort.py
@@ -0,0 +1,20 @@
+#
+# SPDX-FileCopyrightText: Majaahh
+# SPDX-License-Identifier: Apache-2.0
+#
+
+import json
+
+with open("devices.json") as f:
+    data = json.load(f)
+
+with open("devices.json", "w") as f:
+    f.write("{\n")
+
+    items = sorted(data.items())
+
+    for i, (k, v) in enumerate(items):
+        comma = "," if i < len(items) - 1 else ""
+        f.write(f'  "{k}": {json.dumps(v)}{comma}\n')
+
+    f.write("}\n")
